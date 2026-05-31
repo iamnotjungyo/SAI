c37f8520a4b59a5ec3a80e47e27f189a8f60e24e
KimYongBin <adjtny@gmail.com>
2026-05-31 14:37:50 +0900

    회원탈퇴 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import SignupView, LoginView, LogoutView, MyProfileView, UserProfileView
+from .views import SignupView, LoginView, LogoutView, MyProfileView, UserProfileView, UserDeleteView
 
 urlpatterns = [
     # 인증
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path("users/me/", UserDeleteView.as_view(), name="user-delete"),
 
     # 프로필
     path('profile/me/', MyProfileView.as_view(), name='my-profile'),
